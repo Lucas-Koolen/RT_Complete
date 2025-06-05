e3319e7ebcd2dc463aafff151f6a65992a3fac23
Koen van den Elsen <79058750+kvdelsen@users.noreply.github.com>
2025-06-05 12:33:20 +0200

    add code from working detector

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from dashboard import RotationDashboard
+from newDashboard import RotationDashboard
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = RotationDashboard()
-    window.show()
     sys.exit(app.exec_())
+
+
+    # Start de sensor (leest COM7 continue in aparte thread)
+    start_sensor_thread()
+
+    from logic import camera_module
+
+    def frame_callback(img):
+        window.update_frame(img)
+        return img  # geen bewerking
+
+    from PyQt5.QtCore import QTimer
+    QTimer.singleShot(100, lambda: camera_module.start_stream(callback=frame_callback))
+
+    window.show()
+
+    sys.exit(app.exec_())
+
+# This code initializes a PyQt application and displays a dashboard for rotation control.
+#if __name__ == "__main__":
+#    app = QApplication(sys.argv)
+#    window = UltraCalDashboard()
+#    window.show()
+#    sys.exit(app.exec_())
