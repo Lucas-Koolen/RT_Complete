79bd9dcd3b3397e38f6a6bc53f31a61787246851
Koen van den Elsen <79058750+kvdelsen@users.noreply.github.com>
2025-06-05 15:05:53 +0200

    fix storing height as global var

--- logic/newHeightSensor.py
+++ logic/newHeightSensor.py
@@ -11,6 +11,7 @@ def get_latest_height():
     return average_height
 
 def update_height(raw_height):
+    global average_height, _raw_height_buffer
     height = HEIGHT_SCALE * raw_height + HEIGHT_OFFSET
     if 0 <= height <= HEIGHT_MAX_VALID:
         _raw_height_buffer.append(height)
