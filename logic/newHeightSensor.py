517dae84d57c56a1334e2792a4830aeb1d5f6037
Koen van den Elsen <79058750+kvdelsen@users.noreply.github.com>
2025-06-05 14:31:26 +0200

    fix update height function

--- logic/newHeightSensor.py
+++ logic/newHeightSensor.py
@@ -10,13 +10,12 @@ HEIGHT_MAX_VALID = 100  # mm
 def get_latest_height():
     return average_height
 
-def update_height(self, raw_height):
+def update_height(raw_height):
     height = HEIGHT_SCALE * raw_height + HEIGHT_OFFSET
     if 0 <= height <= HEIGHT_MAX_VALID:
         _raw_height_buffer.append(height)
         if len(_raw_height_buffer) > MAX_BUFFER_SIZE:
             _raw_height_buffer.pop(0)
         average_height = round(
-            sum(self._raw_height_buffer) / len(_raw_height_buffer), 1
+            sum(_raw_height_buffer) / len(_raw_height_buffer), 1
         )
-        self.height_label.setText(f"Height: {average_height} mm")
